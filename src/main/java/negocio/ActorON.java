e9084fac40eeff72024b2f5c6f7c8b83848a154e
RubenABAD25 <46006694+RubenABAD25@users.noreply.github.com>
2019-12-09 12:58:25 -0500

    modificacion de ACTORBEAN

---
+++ src/main/java/negocio/ActorON.java
@@ -0,0 +1,44 @@
+package negocio;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import dao.ActorDao;
+import modelo.Actor;
+
+@Stateless
+public class ActorON 
+{
+	@Inject
+	private ActorDao dao;
+	public void guardar(Actor a) throws Exception{
+			
+			if(a.getNombres().length()<5)
+				throw new Exception("Dimension corta");
+			
+			
+			dao.save(a);		
+		}
+		
+		public List<Actor> getListadoActores(){
+			return dao.getActores();
+		}
+		
+		public void borrar(int codigo) throws Exception {
+			try {
+				dao.delete(codigo);
+			}catch(Exception e) {
+				throw new Exception("Error al borrar " + e.getMessage());
+			}
+			
+		}
+		
+		public Actor getActor(int codigo) {
+			Actor aux = dao.read(codigo);
+			return aux;
+			
+		}
+
+}
--- src/main/java/vista/AutorBean.java
+++ src/main/java/vista/AutorBean.java
@@ -1,5 +1,115 @@
 package vista;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.view.facelets.FaceletContext;
+import javax.inject.Inject;
+
+import modelo.Actor;
+import negocio.ActorON;
+
+@ManagedBean
+@ViewScoped
+/**
+ * Esta clase ofrece la intereccion entre Vista-negocio
+ */
 public class AutorBean {
+	
+	//PropietiesBean
+	private String nombre;
+	private Actor ac;
+	private int id;
+	private List<Actor>actores;
+	@Inject
+	private FaceletContext fc;
+	@Inject
+	private ActorON on;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Actor getAc() {
+		return ac;
+	}
+	public void setAc(Actor ac) {
+		this.ac = ac;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Actor> getActores() {
+		return actores;
+	}
+	public void setActores(List<Actor> actores) {
+		this.actores = actores;
+	}
+	@PostConstruct
+	public void init() {
+		System.out.println("init " + ac);
+		actores = on.getListadoActores();
+	}
+
+	/**
+	 * Metodo de guardar Actor
+	 */
+	public String guardar() {
+		try {
+			on.guardar(ac);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * Metodo de editar Actor
+	 */
+	public String editar(int codigo) {
+		System.out.println("codigo editar " + codigo);
+		ac =on.getActor(codigo);
+		System.out.println(ac.toString());
+		
+		return "personas?faces-redirect=true&id="+codigo;
+	}
+	/**
+	 * Metodo de eliminar Actor
+	 */
+	public String borrar(int codigo) {
+		System.out.println("codigo borrar " + codigo);
+		
+		try {
+			on.borrar(codigo);
+			init();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error "+ e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	public void buscar() {
+		if(id==0)
+			return;
+		System.out.println("codigo editar " + this.id);
+		ac = on.getActor(this.id);
+		
+		if(ac == null) {
+			ac = new Actor();
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, 
+					"Registro no existe", "InformaciÃ³n");
+			//((FaceletContext) fc).addMessage(null, msg);
+			
+		}
+	}
 
 }
