202d858b59dfdd23ba5ab54951390ea9306c7ec1
RubenABAD25 <46006694+RubenABAD25@users.noreply.github.com>
2019-12-11 12:03:28 -0500

    change

--- src/main/java/vista/UsuarioBean.java
+++ src/main/java/vista/UsuarioBean.java
@@ -1,9 +1,21 @@
 package vista;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import modelo.Usuario;
+import negocio.UsuarioON;
 
 @ManagedBean
 public class UsuarioBean {
+	@Inject
+	private UsuarioON on;
+	
+	private Usuario us;
+	private List<Usuario>usuario;
+	
 	
 
 }
