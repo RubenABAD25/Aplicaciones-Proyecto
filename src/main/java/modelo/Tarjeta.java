c485248b7077ab801d591c633b36c8ec92e7ede9
DiegoMarcelo1986 <parradiego927@gmail.com>
2019-12-11 11:13:10 -0500

    Cronograma de Actividades en Excel

--- src/main/java/modelo/Tarjeta.java
+++ src/main/java/modelo/Tarjeta.java
@@ -27,6 +27,96 @@ public class Tarjeta {
 	@NotNull
 	@Column(name = "tar_fecha_caducidad")
 	private String fechaCaducidad;
+	
+	@NotNull
+	@Column(name = "tar_codigo_seguridad")
 	private int codigoSeguridad;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNumero() {
+		return numero;
+	}
+
+	/**
+	 * 
+	 * @param numero
+	 */
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+
+	/**
+	 * 
+	 * @param fechaCaducidad
+	 */
+	public void setFechaCaducidad(String fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int getCodigoSeguridad() {
+		return codigoSeguridad;
+	}
+
+	/**
+	 * 
+	 * @param codigoSeguridad
+	 */
+	public void setCodigoSeguridad(int codigoSeguridad) {
+		this.codigoSeguridad = codigoSeguridad;
+	}
+
+	//Mostramos la informacion del objeto Tarjeta
+	@Override
+	public String toString() {
+		return "Tarjeta [id=" + id + ", nombre=" + nombre + ", numero=" + numero + ", fechaCaducidad=" + fechaCaducidad
+				+ ", codigoSeguridad=" + codigoSeguridad + "]";
+	}
 	
 }
